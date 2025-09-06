257e988f85a5c5c7e79dfca749bd574b984769ff
Diana <diana@cloudandcapital.com>
2025-09-06 12:24:39 -0700

    Update embed page

--- next-widget/app/embed/page.tsx
+++ next-widget/app/embed/page.tsx
@@ -1,4 +1,6 @@
+"use client";
 import AiAssistantWidget from "@/components/AiAssistantWidget";
+
 export default function Page() {
   return (
     <div style={{ width: "100%", height: "100vh", background: "transparent" }}>
